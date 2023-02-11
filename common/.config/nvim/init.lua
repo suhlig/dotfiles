5c057f7b25d82761eab225e9eb03658aa5a7ff1c
Steffen Uhlig <steffen@familie-uhlig.net>
2023-02-11 20:58:09 +0100

    Surround with

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -1,3 +1,13 @@
+-- https://stackoverflow.com/a/72517285
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+keymap("v", "\"", ":s/\\%V\\(.*\\)\\%V/\"\\1\"/<CR>", opts)
+keymap("v", "\'", ":s/\\%V\\(.*\\)\\%V/\'\\1\'/<CR>", opts)
+keymap("v", "(", ":s/\\%V\\(.*\\)\\%V/(\\1)/<CR>", opts)
+keymap("v", "[", ":s/\\%V\\(.*\\)\\%V/[\\1]/<CR>", opts)
+keymap("v", "{", ":s/\\%V\\(.*\\)\\%V/{\\1}/<CR>", opts)
+
 -- https://vi.stackexchange.com/a/37427
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
