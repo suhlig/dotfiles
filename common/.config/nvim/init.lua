079ed5914d8fee9092ff52893cc63c3efabc745a
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2023-02-12 12:39:23 +0100

    Keep init.lua clean
    Move customizations to /Users/suhlig/.config/nvim/after/plugin/defaults.lua

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -1,19 +1,3 @@
--- https://stackoverflow.com/a/72517285
-local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
-keymap("v", "\"", ":s/\\%V\\(.*\\)\\%V/\"\\1\"/<CR>", opts)
-keymap("v", "\'", ":s/\\%V\\(.*\\)\\%V/\'\\1\'/<CR>", opts)
-keymap("v", "(", ":s/\\%V\\(.*\\)\\%V/(\\1)/<CR>", opts)
-keymap("v", "[", ":s/\\%V\\(.*\\)\\%V/[\\1]/<CR>", opts)
-keymap("v", "{", ":s/\\%V\\(.*\\)\\%V/{\\1}/<CR>", opts)
-
--- https://vi.stackexchange.com/a/37427
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*" },
-  command = [[%s/\s\+$//e]],
-})
-
 -- Install packer
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 local is_bootstrap = false
